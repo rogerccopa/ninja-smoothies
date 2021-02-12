50197c17f55afcd498cb17980ae1563805c1e1cc
Roger Ccopa <rogerccopa@gmail.com>
2021-02-11 21:51:07 -0500

    Index component

---
+++ src/components/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{smoothie.title}}</h2>
+        <ul class="ingredients">
+          <li v-for="(ingr,index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ingr}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data(){
+    return {
+      smoothies: [
+        {title:'Ninja Brew', slug:'ninja-brew', ingredients:['banana','coffee','milk']},
+        {title:'Morning Mood', slug:'morning-mood', ingredients:['mango','lime','juice']},
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0px;
+}
+.index .ingredients {
+  margin: 30px auto;
+}
+.index .ingredients li {
+  display: inline-block;
+}
+</style>
